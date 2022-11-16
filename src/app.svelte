ebb6ce21de79fa5323ae3e130229ea6e26688d38
nrslmyyy <nurislamiyah370@gmail.com>
2022-11-16 14:58:44 +0800

    svelte

---
+++ src/app.svelte
@@ -0,0 +1,38 @@
+<script>
+    let newItem = '';
+		
+    let todoList = [];
+	
+	function addToList() {
+		todoList = [...todoList, {text: newItem, status: false}];
+		newItem = '';
+	}
+	
+	function removeFromList(index) {
+		todoList.splice(index, 1)
+		todoList = todoList;
+    }
+</script>
+
+<div class="inputList">
+    <input bind:value={newItem} type="text" placeholder="new todo item..">
+    <button on:click={addToList}>Add</button>
+
+    <br/>
+<ul>
+	{#each todoList as item, index}
+	<li>
+	<span >{item.text}</span>
+	<button on:click={() => removeFromList(index)}>Delete</button>
+	<br/>
+	</li>
+    {/each} 
+</ul>
+</div>
+
+<style> 
+	.inputList {
+        display: flex;
+        justify-content: center;
+    }
+</style> 
